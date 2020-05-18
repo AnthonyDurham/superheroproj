04ee655fe922cdbc2f85ec07b68be1e070917f7f
Anthony Durham <durhamusa1313@gmail.com>
2020-05-18 14:04:47 -0500

    first update

---
+++ HeroProj/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HeroProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
